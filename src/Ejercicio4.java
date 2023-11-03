465b8d4884630f7c4b54b370eeeb5a00988cb997
jmarara529 <jmarara529@g.educaand.es>
2023-11-03 14:37:43 +0100

    Ejercicio4.java

---
+++ src/Ejercicio4.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+
+        int anterior;
+        int actual;
+        int diferencia = 0;
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("introduce el recorrido del ascensor:");
+        actual = entrada.nextInt();
+        while (actual != -1){
+            anterior = actual;
+            actual = entrada.nextInt();
+            if (actual != -1) {
+                if (actual != anterior) {
+                    if (actual < anterior) {
+                        for (int recorrido = 0; (recorrido + actual) <= anterior; recorrido++) {
+                            diferencia += recorrido;
+                        }
+                    } else if (actual > anterior) {
+
+                        for (int recorrido = 0; (recorrido + anterior) <= actual; recorrido++) {
+                            diferencia += recorrido;
+                        }
+
+                    }
+                }
+            }
+
+        }
+
+        System.out.println(diferencia);
+
+
+    }
+
+}
